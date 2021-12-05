29ad004d78dd22270af0ba0eb5d61f9300993060
kakeru macbook <kakekakemiya920@g.ecc.u-tokyo.ac.jp>
2021-12-05 20:25:18 +0900

    駅Aと駅Bの間の時間，乗り換え回数，料金を取得するプログラムを実装しました． 詳しくはgetNorikaeTimeのREADME.mdに書きます．

--- getNorikaeTime/main.py
+++ getNorikaeTime/main.py
@@ -1,6 +1,14 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import sys
+args = sys.argv
+
+ekiFrom = args[1]
+ekiTo = args[2]
+
+if (ekiTo == ekiFrom):
+  exit(0)
 
 
 def getTime(data):
@@ -11,12 +19,6 @@ def getTime(data):
     if (0 < len(str_d) < 3):
       ans = str_d
   return ans
-
-
-def getFare(data):
-  data = str(data)
-  # return data.split('分')[0]
-  return re.sub('[^0-9]', '', data)
 
 
 def getTransfer(data):
@@ -31,7 +33,14 @@ def getTransfer(data):
   return ans
 
 
-url = "https://transit.yahoo.co.jp/search/result?from=%E6%9D%B1%E5%A4%A7%E5%89%8D&to=%E5%BE%8C%E6%A5%BD%E5%9C%92&fromgid=&togid=&flatlon=&tlatlon=&via=&viacode=&y=2021&m=12&d=06&hh=12&m1=0&m2=0&type=1&ticket=ic&expkind=1&userpass=1&ws=3&s=0&al=1&shin=1&ex=1&hb=1&lb=1&sr=1&"
+def getFare(data):
+  data = str(data)
+  # return data.split('分')[0]
+  return re.sub('[^0-9]', '', data)
+
+
+url = "https://transit.yahoo.co.jp/search/result?from=" + ekiFrom + "&to=" + ekiTo + "&fromgid=&togid=&flatlon=&tlatlon=&via=&viacode=&y=2021&m=12&d=06&hh=12&m1=0&m2=0&type=1&ticket=ic&expkind=1&userpass=1&ws=3&s=0&al=1&shin=1&ex=1&hb=1&lb=1&sr=1&"
+# url = "https://transit.yahoo.co.jp/search/result?from=%E6%9D%B1%E5%A4%A7%E5%89%8D&to=%E5%BE%8C%E6%A5%BD%E5%9C%92&fromgid=&togid=&flatlon=&tlatlon=&via=&viacode=&y=2021&m=12&d=06&hh=12&m1=0&m2=0&type=1&ticket=ic&expkind=1&userpass=1&ws=3&s=0&al=1&shin=1&ex=1&hb=1&lb=1&sr=1&"
 res = requests.get(url)
 # print(res.text)
 soup = BeautifulSoup(res.text, "html.parser")
@@ -41,10 +50,6 @@ time = soup.find('li', 'time')
 fare = soup.find('li', 'fare')
 transfer = soup.find('li', 'transfer')
 
-print(time)
-print(fare)
-print(transfer)
-
-print(getTime(time))
-print(getFare(fare))
-print(getTransfer(transfer))
+print(ekiFrom, ekiTo, getTime(time), getTransfer(transfer), getFare(fare), sep=',')
+# print(getTransfer(transfer))
+# print(getFare(fare))
