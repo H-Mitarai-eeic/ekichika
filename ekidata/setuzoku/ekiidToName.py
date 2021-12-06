8e8075c462b41d3887087b23e4af90c25ca1372b
kakeru macbook <kakekakemiya920@g.ecc.u-tokyo.ac.jp>
2021-12-06 17:00:03 +0900

    駅とid,路線とidの対応表をつくりました

---
+++ ekidata/setuzoku/ekiidToName.py
@@ -0,0 +1,5 @@
+f = open('tokyoEkidata.csv')
+print("ekicode", "name", sep=",")
+for line in f:
+    l = line.split(',')
+    print(l[0], l[2], sep=",")
---
+++ ekidata/setuzoku/lineToName.py
@@ -0,0 +1,5 @@
+f = open('line20210312free.csv')
+print("ekicode", "name", sep=",")
+for line in f:
+    l = line.split(',')
+    print(l[0], l[4], sep=",")
