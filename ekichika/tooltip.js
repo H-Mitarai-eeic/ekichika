e18226a7fd25256fb6d2904aa16ff2fe2a161a57
Haruki Mitarai <mitarai-haruki520@g.ecc.u-tokyo.ac.jp>
2021-12-16 11:48:21 +0900

    tooltip改良

--- ekichika/tooltip.js
+++ ekichika/tooltip.js
@@ -4,10 +4,10 @@ var stationTooltipScale = 1;  //for tooltip
 var stationTooltipPreviousScale = 1;
 var stationTooltipFontSize = 10;
 
-var stationTooltipBgWidth = 60;
-var stationTooltipBgHeight = 20;
+//var stationTooltipBgWidth = 60;
+//var stationTooltipBgHeight = 20;
 
-var stationTooltipDataN = 2;
+//var stationTooltipDataN = 2;
 
 var stationTooltips = svg.append("g")
 .attr("class", "stationTooltips")
@@ -17,7 +17,7 @@ function tooltipHandlerOnCircle(event){
   var selected_circle = d3.select(this);
   var cx = selected_circle.attr("cx");
   var cy = selected_circle.attr("cy");
-  var data = [selected_circle.datum().name, selected_circle.datum().GroupID];
+  var data = [selected_circle.datum().name];
   var ID = data[0] + data[1];
   var transform = selected_circle.attr("transform");
 
@@ -28,7 +28,7 @@ function tooltipHandlerOnCircle(event){
     hideStationTooltip(event, ID);
   }
 }
-function showStationTooltip(event, data, ID, x, y, transform, y_offset=20, x_offset=0){
+function showStationTooltip(event, data, ID, x, y, transform, y_offset=-10, x_offset=0){
   //var selected_circle = d3.select(this);
 
   //var data_stationTooltip = [selected_circle.datum().GroupID, selected_circle.datum().name]
@@ -38,15 +38,22 @@ function showStationTooltip(event, data, ID, x, y, transform, y_offset=20, x_off
   //var circle_transform = selected_circle.attr("transform");
   //var y_offset = 20;
 
-  var y_col = y - y_offset / Math.cbrt(stationTooltipScale);
-  var x_col = x - x_offset / Math.cbrt(stationTooltipScale);
+  var y_col = parseFloat(y) + y_offset / Math.cbrt(stationTooltipScale);
+  var x_col = parseFloat(x) + x_offset / Math.cbrt(stationTooltipScale);
   var scaled_fontsize = stationTooltipFontSize / Math.cbrt(stationTooltipScale);
   var dataN = data.length;
+  var textMaxLength = 0;
+  var stationTooltipBgWidth;
+  for(var i = 0; i < dataN; i++){
+    if (textMaxLength < data[i].length){
+      textMaxLength = data[i].length;
+    }
+  }
+  stationTooltipBgWidth = scaled_fontsize * textMaxLength * 1.2;
 
   stationTooltip = stationTooltips
       .append("g")
       .attr("class", "stationTooltip" + ID)
-      //.attr("transform", transform)
       .call(zoom);
   //console.log(stationTooltipZoom);
   stationTooltip.selectAll(".stationTooltipText")
@@ -54,11 +61,12 @@ function showStationTooltip(event, data, ID, x, y, transform, y_offset=20, x_off
       .enter()
       .append("text")
       .attr("class", "stationTooltipText")
-      .attr("fill", "black")
+      .attr("fill", "white")
       .attr("stroke", "none")
       .attr("text-anchor", "middle")
       .attr("x", x_col)
-      .attr("y", function(d, i){return scaled_fontsize * i + y_col;})
+      .attr("y", y_col)
+      .attr("dy", function(d, i){return - scaled_fontsize * (dataN - i - 1); })
       .attr("font-size", scaled_fontsize)
       .text(function(d){return d; })
       .call(zoom);
@@ -67,13 +75,15 @@ function showStationTooltip(event, data, ID, x, y, transform, y_offset=20, x_off
   stationTooltip
       .append("rect")
       .attr("class", "stationTooltipBg")
-      .attr("width", stationTooltipBgWidth / Math.cbrt(stationTooltipScale))
-      .attr("height", scaled_fontsize * dataN + 1)
-      .attr("x", x_col - stationTooltipBgWidth/ Math.cbrt(stationTooltipScale) /2)
-      .attr("y", y_col - scaled_fontsize)
-      .attr("fill", "white")
+      .attr("width", stationTooltipBgWidth)
+      .attr("height", scaled_fontsize * dataN * 1.2)
+      .attr("x", x_col - stationTooltipBgWidth /2)
+      .attr("y", y_col - scaled_fontsize * dataN)
+      .attr("rx", 1)
+      .attr("rx", 1)
+      .attr("fill", "black")
       .attr("fill-opacity", 0.1)
-      .attr("stroke", "black")
+      .attr("stroke", "white")
       .attr("stroke-width", 0.1);
 }
 function hideStationTooltip(event, ID){
@@ -81,26 +91,41 @@ function hideStationTooltip(event, ID){
 }
 function resizeStationTooltip(event){
   stationTooltipScale = event.transform.k;
-  var y_offset = 20;
+  var y_offset = -10;
+
   var scaled_fontsize = stationTooltipFontSize / Math.cbrt(stationTooltipScale);
-  console.log(stationTooltips.selectAll(".stationTooltipText").empty());
+  var scaled_previous_fontsize = stationTooltipFontSize / Math.cbrt(stationTooltipPreviousScale);
+
+  var scaled_y_offset = y_offset / Math.cbrt(stationTooltipScale);
+  var scaled_previous_y_offset = y_offset / Math.cbrt(stationTooltipPreviousScale);
+
   if (stationTooltips != undefined){
     stationTooltips.attr("transform", event.transform);
   }
   if(stationTooltips != undefined && stationTooltips.selectAll(".stationTooltipText").empty() != true){
-    var current_y = parseFloat(stationTooltips.selectAll(".stationTooltipText").attr("y"));
-    var current_x = parseFloat(stationTooltips.selectAll(".stationTooltipBg").attr("x"));
-    
-    stationTooltip.selectAll(".stationTooltipText")
+    var dataN = 1.0;
+    stationTooltips.selectAll(".stationTooltipText")
               .attr("font-size", scaled_fontsize)
               .attr("y", function(d, i){
-                return current_y - y_offset / Math.cbrt(stationTooltipScale) + y_offset / Math.cbrt(stationTooltipPreviousScale) + i * stationTooltipFontSize / Math.cbrt(stationTooltipScale);
-              });
-    stationTooltip.selectAll(".stationTooltipBg")
-              .attr("x", current_x + stationTooltipBgWidth/ Math.cbrt(stationTooltipPreviousScale) /2 - stationTooltipBgWidth/ Math.cbrt(stationTooltipScale) /2)
-              .attr("y", current_y - y_offset / Math.cbrt(stationTooltipScale) + y_offset / Math.cbrt(stationTooltipPreviousScale) - scaled_fontsize)
-              .attr("width", stationTooltipBgWidth / Math.cbrt(stationTooltipScale))
-              .attr("height", scaled_fontsize * stationTooltipDataN + 1);
+                var current_y = parseFloat(d3.select(this).attr("y"));
+                return current_y + scaled_y_offset - scaled_previous_y_offset;
+              })
+              .attr("dy", function(d, i){return - scaled_fontsize * (dataN - i - 1); });
+    stationTooltips.selectAll(".stationTooltipBg")
+              .attr("width", function(){
+                var current_width = parseFloat(d3.select(this).attr("width"));
+                return current_width * scaled_fontsize / scaled_previous_fontsize; 
+              })
+              .attr("height", scaled_fontsize * dataN * 1.2)
+              .attr("x", function(){
+                var current_x = parseFloat(d3.select(this).attr("x"));
+                var current_width = parseFloat(d3.select(this).attr("width"));
+                return current_x - current_width * scaled_fontsize /scaled_previous_fontsize/2 + current_width / 2;
+              })
+              .attr("y", function(){
+                var current_y = parseFloat(d3.select(this).attr("y"));
+                return current_y + scaled_y_offset - scaled_previous_y_offset - scaled_fontsize * dataN + scaled_previous_fontsize * dataN;
+              })
   }
   stationTooltipPreviousScale = stationTooltipScale;
 }
