9e5df1a800fb72626c4fb2db2f8b6dfe1ce38f78
kakeru macbook <kakekakemiya920@g.ecc.u-tokyo.ac.jp>
2021-12-16 14:45:07 +0900

    検索結果駅表示のベースを作りました 次はJSと組み合わせます

--- ekichika/menu.js
+++ ekichika/menu.js
@@ -17,9 +17,12 @@ function onMenuChange() {
 
   if (mode == "one") {
     document.getElementById("clockAndTime").style.display = "block";
+    document.getElementById("goalStations").style.display = "none";
   } else if (mode == "meet") {
     document.getElementById("clockAndTime").style.display = "none";
+    document.getElementById("goalStations").style.display = "block";
   } else if (mode == "meetWithTime") {
     document.getElementById("clockAndTime").style.display = "block";
+    document.getElementById("goalStations").style.display = "none";
   }
 }
